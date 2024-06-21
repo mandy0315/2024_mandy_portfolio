8d7ff4a7e6e441a1074f603623820423496dc8c3
mandy <mandy3152618@gmail.com>
2024-06-21 15:23:13 +0800

    build: iconify & compontents.d.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import IconsResolver from "unplugin-icons/resolver";
+import Icons from "unplugin-icons/vite";
+import Components from "unplugin-vue-components/vite";
+
 export default defineNuxtConfig({
   devtools: { enabled: true }, // Enable Vue Devtools
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "unplugin-icons/nuxt"],
   typescript: { strict: false },
   css: ["@/assets/styles/base.scss"],
   postcss: {
@@ -9,5 +12,13 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [IconsResolver()],
+      }),
+      Icons({}),
+    ],
   },
 });
