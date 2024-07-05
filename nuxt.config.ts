c0137d86e42cf4fd959ac2fecb8b9becbe7be78c
mandy <mandy3152618@gmail.com>
2024-07-05 13:49:18 +0800

    delete: tailwind 套件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,20 +3,10 @@ import Icons from "unplugin-icons/vite";
 import Components from "unplugin-vue-components/vite";
 
 export default defineNuxtConfig({
-  // Enable Vue Devtools
-  devtools: { enabled: true },
-
+  devtools: { enabled: true }, // Enable Vue Devtools
   modules: ["@nuxt/eslint", "unplugin-icons/nuxt", "nuxt-svgo"],
   typescript: { strict: false },
   css: ["@/assets/styles/base.scss"],
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-
   vite: {
     plugins: [
       Components({
@@ -36,11 +26,9 @@ export default defineNuxtConfig({
       },
     },
   },
-
   svgo: {
     autoImportPath: "@/assets/icons/",
     componentPrefix: "svg",
   },
-
   compatibilityDate: "2024-07-04",
 });
